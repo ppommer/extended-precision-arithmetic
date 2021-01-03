9eef2db3d7dec5c5da9c885b38e10ff1aa9d7e16
Paul Pommer <50099454+ppommer@users.noreply.github.com>
2021-01-03 20:09:53 +0100

    Update fix_vec_dot.asm

--- fix_vec_dot.asm
+++ fix_vec_dot.asm
@@ -1,7 +1,3 @@
-%ifndef era
-%include "io.inc"
-%endif
-
 section .bss
     v1: resd 6
     v2: resd 6
@@ -10,27 +6,7 @@ global fix_vec_dot_asm
 extern fix_mul_asm
 extern fix_add_asm
 
-%ifndef era
-global main
-%endif
-
 section .text
-
-;You can add stuff to main as well
-%ifndef era
-main:    
-    ; write your own test code here
-    ; missing: set values in v1
-    ; missing: set values in v2
-    push dword v2
-    push dword v1
-    call fix_vec_dot_asm
-    add esp, 8
-    ;result in edx:eax
-    ret
-%endif
-
-;ASSIGNMENT START: fix_vec_dot
 
 fix_vec_dot_asm:
 
@@ -126,5 +102,3 @@ fix_vec_dot_asm:
     pop ebp
     
     ret
-
-;ASSIGNMENT END: fix_vec_dot
