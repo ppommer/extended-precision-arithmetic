95a210e7fbb57dd8c69ebb2f6e7a57ec1df939e4
Paul Pommer <50099454+ppommer@users.noreply.github.com>
2021-01-03 20:10:27 +0100

    Update fix_vec_sub.asm

--- fix_vec_sub.asm
+++ fix_vec_sub.asm
@@ -1,7 +1,3 @@
-%ifndef era
-%include "io.inc"
-%endif
-
 section .bss
     v1: resd 6
     v2: resd 6
@@ -10,28 +6,7 @@ section .bss
 section .data
 global fix_vec_sub_asm
 
-%ifndef era
-global main
-%endif
-
 section .text
-
-;You can add stuff to main as well
-%ifndef era
-main:
-    ; write your own test code here
-    ; missing: set values in v1
-    ; missing: set values in v2
-    push dword o
-    push dword v2
-    push dword v1
-    call fix_vec_sub_asm
-    add esp, 12
-    ;o = v1 - v2
-    ret
-%endif
-
-;ASSIGNMENT START: fix_vec_sub
 
 fix_vec_sub_asm:
 
@@ -85,6 +60,3 @@ fix_vec_sub_asm:
         pop ebp
 
     ret
-
-;ASSIGNMENT END: fix_vec_sub
-
